ae59653fd810f638abcb2ff0d8e234e1cf26eaca
dotNulled <20d.dhruv@gmail.com>
2024-07-31 19:41:25 +0530

    Initial commit

---
+++ v1-app/app/index.tsx
@@ -0,0 +1,52 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View>
+      <center><h1><Text>ASSIGN</Text></h1>
+        <table>
+          <tr>
+            <th><Text>Number</Text></th>
+            <th><Text>Class</Text></th>
+          </tr>
+          <tr>
+            <td><Text>I</Text></td>
+            <td><Text>Class 1</Text></td>
+          </tr>
+          <tr>
+            <td><Text>II</Text></td>
+            <td><Text>Class 2</Text></td>
+          </tr>
+          <tr>
+            <td><Text>III</Text></td>
+            <td><Text>Class 3</Text></td>
+          </tr>
+          <tr>
+            <td><Text>IV</Text></td>
+            <td><Text>Class 4</Text></td>
+          </tr>
+          <tr>
+            <td><Text>V</Text></td>
+            <td><Text>Class 5</Text></td>
+          </tr>
+          <tr>
+            <td><Text>VI</Text></td>
+            <td><Text>Class 6</Text></td>
+          </tr>
+          <tr>
+            <td><Text>VII</Text></td>
+            <td><Text>Class 7</Text></td>
+          </tr>
+          <tr>
+            <td><Text>VIII</Text></td>
+            <td><Text>Class 8</Text></td>
+          </tr>
+          <tr>
+            <td><Text>IX</Text></td>
+            <td><Text>Class 9</Text></td>
+          </tr>
+        </table>
+      </center>
+    </View>
+  );
+}
